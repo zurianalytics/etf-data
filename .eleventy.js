91cc47857dff391354610dc5ca785ae7df6e7978
Slavin Donchev <zuri-analytics@protonmail.com>
2020-05-24 04:58:30 -0700

    Stuff

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     // Add passthrough file copy of generated dir
     eleventyConfig.addPassthroughCopy({"src/images": "images"});
     eleventyConfig.addPassthroughCopy({"src/js/*external.js": "js"});
+    eleventyConfig.addPassthroughCopy("CNAME");
 
     return {
       passthroughFileCopy: true
