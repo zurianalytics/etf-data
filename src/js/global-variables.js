dfb0514f3b3d6d09dbbd4da82280e16f7f49a530
Slavin Donchev <zuri-analytics@protonmail.com>
2020-05-24 04:53:02 -0700

    Added proper url

--- src/js/global-variables.js
+++ src/js/global-variables.js
@@ -2,8 +2,8 @@ module.exports =
 {
     env:
     {
-        //api: "https://test.api.etf.zurianalytics.com",
-        api: "http://127.0.0.1:8666",
+        api: "https://test.api.etf.zurianalytics.com",
+        //api: "http://127.0.0.1:8666",
         name: "Zuri Analytics API"
     }
 }
