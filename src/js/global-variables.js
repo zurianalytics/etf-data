53e02b7439ad87eae2d5066825b4c1a5d6ad10bf
Slavin Donchev <zuri-analytics@protonmail.com>
2020-05-24 07:55:43 -0700

    Minor improvements

--- src/js/global-variables.js
+++ src/js/global-variables.js
@@ -4,6 +4,6 @@ module.exports =
     {
         api: "https://test.api.etf.zurianalytics.com",
         //api: "http://127.0.0.1:8666",
-        name: "Zuri Analytics API"
+        name: "ETF Data API"
     }
 }
