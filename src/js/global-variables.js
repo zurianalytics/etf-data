a41fa47fb48d14ea7db8dd503b38f69aac0526b7
Slavin Donchev <zuri-analytics@protonmail.com>
2020-05-25 14:49:18 -0700

    Changed subscription method

--- src/js/global-variables.js
+++ src/js/global-variables.js
@@ -4,6 +4,8 @@ module.exports =
     {
         api: "https://test.api.etf.zurianalytics.com",
         //api: "http://127.0.0.1:8666",
-        name: "ETF Data API"
+        website: "https://api.etf-data.com/",
+        //website: "http://localhost:8080",
+        name: "ETF Data API",
     }
 }
