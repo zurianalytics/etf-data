507a15daad39632d374c8569f901d1b715cbc3e4
Slavin Donchev <zuri-analytics@protonmail.com>
2020-07-03 18:34:58 +0200

    opravqme neshtoto

--- src/js/global-variables.js
+++ src/js/global-variables.js
@@ -4,8 +4,8 @@ module.exports =
     {
         api: "https://test.api.etf-data.com",
         //api: "http://127.0.0.1:8666",
-        //website: "https://etf-data.com/",
-        website: "http://localhost:8080",
+        website: "https://etf-data.com/",
+        //website: "http://localhost:8080",
         name: "ETF Data API",
     }
 }
