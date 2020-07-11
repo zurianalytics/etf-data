9876a9c177b7b7e4d948118fe0876d0d0427568b
Slavin Donchev <zuri-analytics@protonmail.com>
2020-07-11 17:56:58 +0200

    Switched to prod

--- src/js/global-variables.js
+++ src/js/global-variables.js
@@ -2,7 +2,7 @@ module.exports =
 {
     env:
     {
-        api: "https://test.api.etf-data.com",
+        api: "https://api.etf-data.com",
         //api: "http://127.0.0.1:8666",
         website: "https://etf-data.com/",
         //website: "http://localhost:8080",
